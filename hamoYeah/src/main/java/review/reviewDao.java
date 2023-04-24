427caf86db869a86d2cc8d17591c5d7fe96666ee
Heesoo KIM <Heesoo KIM@DESKTOP-22UVKHC>
2023-04-24 12:25:16 +0900

    reveiw_vo, dao, service

---
+++ hamoYeah/src/main/java/review/reviewDao.java
@@ -0,0 +1,167 @@
+package review;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import conn.DBConnect;
+
+public class reviewDao {
+	private DBConnect dbconn;
+
+	public reviewDao() {
+		dbconn = DBConnect.getInstance();
+	}
+	
+	//리뷰작성(add)
+	public void insert(reviewVo vo) {
+		Connection conn = dbconn.conn();
+		String sql = "insert into H_review values(?,seq_H_review.nextval, ?, sysdate,0,?,?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getMember_id());
+			pstmt.setInt(2, vo.getBoard_num());
+			pstmt.setString(3, vo.getImagepath());
+			pstmt.setString(4, vo.getContent());
+
+			int num = pstmt.executeUpdate();
+			System.out.println(num + " 줄이 추가되었다");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+//후기수정(edit)	primary key(review_num)로 내용(content) 수정
+	public void update(reviewVo vo) {
+		Connection conn = dbconn.conn();
+		String sql = "update H_review set content=?, imagepath=? where review_num=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getContent());
+			pstmt.setString(2, vo.getImagepath());
+			pstmt.setString(3, vo.getMember_id());
+			int num = pstmt.executeUpdate();
+			System.out.println(num + " 줄이 수정됨");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// delete: primary key(review_num)로 찾아서 삭제
+	public void delete(int review_num) {
+		Connection conn = dbconn.conn();
+		String sql = "delete from H_review where review_num=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, review_num);
+			int num = pstmt.executeUpdate();
+			System.out.println(num + " 줄이 삭제됨");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//selectAll(review 전체목록 띄우기)
+	public ArrayList<reviewVo> selectAll(){
+		Connection conn = dbconn.conn();
+		ArrayList<reviewVo> list = new ArrayList<>();
+		String sql = "select * from H_review order by review_num desc";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(new reviewVo(rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getDate(4), rs.getInt(5), rs.getString(6), rs.getString(7)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	
+	//selectByTag(Tag로 검색)
+	public reviewVo selectByTag(String tag) {
+		Connection conn = dbconn.conn();
+		String sql = "select * from H_review where tag=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, tag);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return new reviewVo(rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getDate(4), rs.getInt(5), rs.getString(6), rs.getString(7));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	
+	//selectByMember(작성자로 검색)
+	public ArrayList<reviewVo> selectByMemberId(String member_id){
+		Connection conn = dbconn.conn();
+		ArrayList<reviewVo> list = new ArrayList<>();
+		String sql = "select * from H_review where member_id=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(new reviewVo(rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getDate(4), rs.getInt(5), rs.getString(6), rs.getString(7)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+}
--- hamoYeah/src/main/java/review/reviewVo.java
+++ hamoYeah/src/main/java/review/reviewVo.java
@@ -1,5 +1,75 @@
 package review;
 
-public class reviewVo {
+import java.sql.Date;
 
+public class reviewVo {
+	private String member_id;
+	private int review_num;
+	private int board_num;
+	private Date r_date;
+	private int likes;
+	private String imagepath;
+	private String content;
+	
+	
+	public reviewVo() {};
+	
+	public reviewVo(String member_id, int review_num, int board_num, Date r_date, int likes, String imagepath,
+			String content) {
+		super();
+		this.member_id = member_id;
+		this.review_num = review_num;
+		this.board_num = board_num;
+		this.r_date = r_date;
+		this.likes = likes;
+		this.imagepath = imagepath;
+		this.content = content;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public int getReview_num() {
+		return review_num;
+	}
+	public void setReview_num(int review_num) {
+		this.review_num = review_num;
+	}
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+	public Date getR_date() {
+		return r_date;
+	}
+	public void setR_date(Date r_date) {
+		this.r_date = r_date;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public String getImagepath() {
+		return imagepath;
+	}
+	public void setImagepath(String imagepath) {
+		this.imagepath = imagepath;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "reviewVo [member_id=" + member_id + ", review_num=" + review_num + ", board_num=" + board_num
+				+ ", r_date=" + r_date + ", likes=" + likes + ", imagepath=" + imagepath + ", content=" + content + "]";
+	}
 }
